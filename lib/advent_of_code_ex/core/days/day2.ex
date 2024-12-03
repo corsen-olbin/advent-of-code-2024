612aa58a125eae9888b1091d0763520e175c8c9a
corsen-olbin <corbinolsen97@gmail.com>
2024-12-02 19:17:47 -0600

    day 2

--- lib/advent_of_code_ex/core/days/day2.ex
+++ lib/advent_of_code_ex/core/days/day2.ex
@@ -1,10 +1,47 @@
 defmodule AdventOfCodeEx.Core.Days.Day2 do
 
-  def part_1(_input) do
-    :unimplemented
+  def part_1(input) do
+    input
+    |> read_input
+    |> num_of_safe_lists(&is_safe/1)
   end
 
-  def part_2(_input) do
-    :unimplemented
+  def part_2(input) do
+    input
+    |> read_input
+    |> num_of_safe_lists(&is_safe_2/1)
+  end
+
+  def read_input(input) do
+    input
+    |> String.split("\r\n")
+    |> Enum.map(fn x -> String.split(x, " ") |> Enum.map(&String.to_integer/1) end)
+  end
+
+  def num_of_safe_lists(lists, is_safe) do
+    lists
+    |> Enum.count(is_safe)
+  end
+
+  def is_safe(list) do
+    diffs =
+    list
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(fn [x1, x2] -> x1 - x2 end)
+
+    (Enum.all?(diffs, &(&1 < 0)) or Enum.all?(diffs, &(&1 > 0)))
+    and Enum.all?(diffs, &(-4 < &1 and &1 < 4))
+  end
+
+  def is_safe_2(list) do
+    is_safe_2_rec(list, list, 0, Enum.count(list))
+  end
+
+  def is_safe_2_rec(orig_list, list, index, count) do
+    cond do
+      is_safe(list) -> true
+      index >= count -> false
+      true -> is_safe_2_rec(orig_list, List.delete_at(orig_list, index), index + 1, count)
+    end
   end
 end
